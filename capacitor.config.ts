7c734b6113ddd273863ba383fb20ff900be1173f
Fernando Parra <thefer767@gmail.com>
2023-02-02 18:43:34 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FrontEnd-Proyecto1-Moviles',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
